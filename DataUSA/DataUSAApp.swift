7972f6ef369a5f55fcf7ef9ba2ec77100a4ce2cb
Jorge O'Neill <jorge.oneill@me.com>
2024-08-03 21:36:06 +0100

    Initial Commit

---
+++ DataUSA/DataUSAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataUSAApp.swift
+//  DataUSA
+//
+//  Created by Jorge O'Neill on 03/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DataUSAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
